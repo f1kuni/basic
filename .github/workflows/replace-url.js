92ffc0d5be50944490ab4516bb0fe347b32b5037
naoki <n.globe.us@gmail.com>
2021-10-22 19:31:42 +0900

    some fix

--- .github/workflows/replace-url.js
+++ .github/workflows/replace-url.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 
-const yml = fs.readFileSync(path.resolve(process.env.GITHUB_WORKSPACE, 'style.yml'), {encoding: 'utf-8'})
-const data = yml.replace(/\$GITHUB_ACTOR/g, process.env.GITHUB_ACTOR)
-fs.writeFileSync(path.resolve(process.env.GITHUB_WORKSPACE, 'style.yml'), data)
+const GITHUB_WORKSPACE = process.argv[2];
+
+const yml = fs.readFileSync(path.resolve(GITHUB_WORKSPACE, 'style.yml'), {encoding: 'utf-8'})
+const data = yml.replace(/\$GITHUB_REPOSITORY/g, process.env.GITHUB_REPOSITORY)
+fs.writeFileSync(path.resolve(GITHUB_WORKSPACE, 'style.yml'), data)
